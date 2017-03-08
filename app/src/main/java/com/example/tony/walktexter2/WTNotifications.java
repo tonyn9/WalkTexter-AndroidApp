569664578709de9a7ffa23ee81ece4730d251717
Tony Nguyen <tonyn9@uci.edu>
2017-03-07 21:52:30 -0800

    added a warning picture and updated notifications to reflect that

--- app/src/main/java/com/example/tony/walktexter2/WTNotifications.java
+++ app/src/main/java/com/example/tony/walktexter2/WTNotifications.java
@@ -48,14 +48,16 @@ public class WTNotifications {
         // This image is used as the notification's large icon (thumbnail).
         // TODO: Remove this if your notification has no relevant thumbnail.
 
+        final Bitmap picture;
+
         String[] arr = exampleString.split(".");
 
         if ( arr[0].equals("Walk")){
-            final Bitmap picture = BitmapFactory.decodeResource(res, R.drawable.gosign);
+            picture = BitmapFactory.decodeResource(res, R.drawable.gosign);
         } else if( arr[0].equals("Don't Walk.")){
-            final Bitmap picture = BitmapFactory.decodeResource(res, R.drawable.stopsign);
+            picture = BitmapFactory.decodeResource(res, R.drawable.stopsign);
         }else{
-            final Bitmap picture = BitmapFactory.decodeResource(res, R.drawable.example_picture);
+            picture = BitmapFactory.decodeResource(res, R.drawable.warning_sign);
         }
 
 
