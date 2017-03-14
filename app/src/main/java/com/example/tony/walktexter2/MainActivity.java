8682f611bb2d2034abb25f3e6b7bd7113516f9de
Linda Vang <lvang2@uci.edu>
2017-03-14 04:38:46 -0700

    Added Floating Action Button to link to our team's website

--- app/src/main/java/com/example/tony/walktexter2/MainActivity.java
+++ app/src/main/java/com/example/tony/walktexter2/MainActivity.java
@@ -1,16 +1,63 @@
 package com.example.tony.walktexter2;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Floating action buttons for plus and moreinfo
+    FloatingActionButton fab_plus, fab_moreinfo;
+    Animation FabOpen, FabClose, FabClockwise, FabCounterclockwise;
+    boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /********** For Floating Action Buttons **********/
+
+        fab_plus = (FloatingActionButton)findViewById(R.id.fab_plus);
+        fab_moreinfo = (FloatingActionButton)findViewById(R.id.fab_moreinfo);
+
+        FabOpen = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open);
+        FabClose = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        FabClockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_clockwise);
+        FabCounterclockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_counterclockwise);
+
+        fab_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (isOpen) {
+                    fab_moreinfo.startAnimation(FabClose);
+                    fab_plus.startAnimation(FabCounterclockwise);
+                    fab_moreinfo.setClickable(false);
+                    isOpen = false;
+                } else {
+                    fab_moreinfo.startAnimation(FabOpen);
+                    fab_plus.startAnimation(FabClockwise);
+                    fab_moreinfo.setClickable(true);
+                    isOpen = true;
+                }
+            }
+        });
+
+        fab_moreinfo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent webpageIntent = getOpenWebpageIntent(MainActivity.this);
+                startActivity(webpageIntent);
+            }
+        });
+        /********** End of For Floating Action Buttons **********/
+
     }
 
     //start the service
@@ -23,4 +70,19 @@ public class MainActivity extends AppCompatActivity {
         stopService(new Intent(this, WTService.class));
         finish();
     }
+
+    /********** For Floating Action Buttons -- MoreInfo **********/
+    public static Intent getOpenWebpageIntent(Context context) {
+
+        try {
+            //context.getPackageManager()
+              //      .getPackageInfo("com.twitter.android", 0); //Checks if app is installed.
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://srproj.eecs.uci.edu/projects/project-group-60-walktexter")); //Trys to make intent with webpage's URI
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://srproj.eecs.uci.edu/projects/project-group-60-walktexter")); //catches and opens a url to the desired page
+        }
+    }
+    /********** End of For Floating Action Buttons -- MoreInfo **********/
 }
